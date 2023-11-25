a90999605e0895d587189541d3f5aa8956fac821
Drake Davis <drakedavis0203@gmail.com>
2023-11-25 00:30:26 -0500

    Added a function to categorize telecom users into different plans ('Premium', 'Unlimited', 'Basic') based on their monthly data usage, voice call minutes, and text messages. The function evaluates usage criteria to assign the appropriate plan category. This enhancement will facilitate the simulation of realistic telecom user behavior over the 12 months of 2024, with the list of months already initialized in the script.

--- dataSim2.py
+++ dataSim2.py
@@ -32,3 +32,33 @@ user_profiles = np.column_stack((user_names, user_types))
 # Print sample user profiles
 print("Sample User Profiles:")
 print(user_profiles[:5])
+
+# List of 12 months in year 2024
+months = [
+    "Jan2024", "Feb2024", "Mar2024", "Apr2024",
+    "May2024", "Jun2024", "Jul2024", "Aug2024",
+    "Sep2024", "Oct2024", "Nov2024", "Dec2024"
+]
+
+# Initialize empty list for monthly data
+monthly_usage_data = []
+
+# Defines function, assigns 3 different tiers conditioned on data usage per user
+def plan_category(data_usage, voice_call_minutes, text_messages):
+    """
+    Categorizes a telecom user into a plan based on their usage data.
+
+    Parameters:
+    data_usage (float): The amount of data used by the user in GB.
+    voice_call_minutes (int): Total minutes of voice calls made by the user.
+    text_messages (int): Number of text messages sent by the user.
+
+    Returns:
+    str: The category of the plan - 'Premium', 'Unlimited', or 'Basic'.
+    """
+    if data_usage >= 251 and data_usage <= 799:
+        return "Premium"
+    elif data_usage >= 800 or voice_call_minutes >= 2001 or text_messages >= 3750:
+        return "Unlimited"
+    else:
+        return "Basic"
